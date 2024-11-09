2486027c2d022d1dd3770a72392e9113da8da610
髙野 晃 <takano.akira@temona.co.jp>
2024-11-09 15:52:51 +0900

    【1-7】ルーティングを追加しよう

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/flosch/pongo2"
@@ -14,8 +15,11 @@ const tmplPath = "src/template/"
 var e = createMux()
 
 func main() {
-	// `/` というパス（URL）と `articleIndex` という処理を結びつける
+	// ルーティング設定
 	e.GET("/", articleIndex)
+	e.GET("/new", articleNew)
+	e.GET("/:id", articleShow)
+	e.GET("/:id/edit", articleEdit)
 
 	// Webサーバーをポート番号 8080 で起動する
 	e.Logger.Fatal(e.Start(":8080"))
@@ -41,10 +45,45 @@ func createMux() *echo.Echo {
 // ハンドラ関数でテンプレファイルとデータを指定してrender()関数を呼び出す。
 func articleIndex(c echo.Context) error {
 	data := map[string]interface{}{
-		"Message": "Hello, World!",
+		"Message": "Article Index",
 		"Now":     time.Now(),
 	}
 	return render(c, "article/index.html", data)
+}
+
+func articleNew(c echo.Context) error {
+	data := map[string]interface{}{
+		"Message": "Article New",
+		"Now":     time.Now(),
+	}
+
+	return render(c, "article/new.html", data)
+}
+
+func articleShow(c echo.Context) error {
+	// パスパラメータを抽出
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	data := map[string]interface{}{
+		"Message": "Article Show",
+		"Now":     time.Now(),
+		"ID":      id,
+	}
+
+	return render(c, "article/show.html", data)
+}
+
+func articleEdit(c echo.Context) error {
+	// パスパラメータを抽出
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	data := map[string]interface{}{
+		"Message": "Article Edit",
+		"Now":     time.Now(),
+		"ID":      id,
+	}
+
+	return render(c, "article/edit.html", data)
 }
 
 // ここでHTMLのバイトデータを作る
