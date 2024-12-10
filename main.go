9507b1e06c833492269968df98d87dca350a6d21
desuken5963 <desuken.5963@gmail.com>
2024-12-10 14:53:03 +0900

    もっと見る機能実装

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 	"go_blog/handler"
 	"go_blog/repository"
 
+	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql" // Using MySQL driver
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/go-playground/validator/v10"
 )
 
 var db *sqlx.DB
@@ -22,12 +22,22 @@ func main() {
 	repository.SetDB(db)
 
 	// ルーティング設定
+	// TOP ページに記事の一覧を表示します。
 	e.GET("/", handler.ArticleIndex)
-	e.GET("/new", handler.ArticleNew)
-	e.GET("/:id", handler.ArticleShow)
-	e.GET("/:id/edit", handler.ArticleEdit)
-	e.POST("/", handler.ArticleCreate)
-	e.DELETE("/:id", handler.ArticleDelete)
+
+	// 記事に関するページは "/articles" で開始するようにします。
+	// 記事一覧画面には "/" と "/articles" の両方でアクセスできるようにします。
+	// パスパラメータの ":id" も ":articleID" と明確にしています。
+	e.GET("/articles", handler.ArticleIndex)                // 一覧画面
+	e.GET("/articles/new", handler.ArticleNew)              // 新規作成画面
+	e.GET("/articles/:articleID", handler.ArticleShow)      // 詳細画面
+	e.GET("/articles/:articleID/edit", handler.ArticleEdit) // 編集画面
+
+	// HTML ではなく JSON を返却する処理は "/api" で開始するようにします。
+	// 記事に関する処理なので "/articles" を続けます。
+	e.GET("/api/articles", handler.ArticleList)                 // 一覧
+	e.POST("/api/articles", handler.ArticleCreate)              // 作成
+	e.DELETE("/api/articles/:articleID", handler.ArticleDelete) // 削除
 
 	// Webサーバーをポート番号 8080 で起動する
 	e.Logger.Fatal(e.Start(":8080"))
